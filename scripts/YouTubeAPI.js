532e064316745a0cfbdb3899a1de9dfb078d3885
Aldy <aldertonz@live.co.uk>
2023-01-09 18:43:19 +0000

    fgd
    gd

---
+++ scripts/YouTubeAPI.js
@@ -0,0 +1,12 @@
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCXbkqa4ImVZr2HkSmoZnVvg&maxResults=10&order=date&key=AIzaSyCKMFkhcRxgp4vmkBn5YXrAdFqgv8S7Zok")
+.then((result)=>{
+    return result.json()
+}).then((data)=>{
+    let videos = data.items
+    let videoContainer = document.querySelector(".youtube-container")
+    for(video of videos){
+        videoContainer.innerHTML += '
+            <img src="${video.snippet.thumbnailshigh.url}">
+        '
+    }
+})
