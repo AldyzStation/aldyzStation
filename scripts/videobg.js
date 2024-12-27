7ce1a10e44eeab2cdf9a935088e08c760ed2bbf7
Aldyz <34462226+AldyzStation@users.noreply.github.com>
2024-12-27 15:53:00 +0000

    .

--- scripts/videobg.js
+++ scripts/videobg.js
@@ -2,11 +2,11 @@ const video = document.getElementById("myVideo");
 const btn = document.getElementById("myPlay");
 
 function myPlay() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
 }
